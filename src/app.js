9a0624886a68a7e19ce6eddf0e5068afc6af49de
BlitzSlashes <63169496+BlitzSlashes@users.noreply.github.com>
2021-03-03 09:31:38 +0200

    Updated packages

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const db = require('./db/db.js');
 const userRoutes = require("./routers/user.js");
 const taskRoutes = require("./routers/task.js");
 
+// /Users/Guntis/mongodb/bin/mongod.exe --dbpath=/Users/Guntis/mongodb-data
+
 // Project variables
 const app = express();
 
